23585ba8be5e09d8fd74a0399e352c7a4a30a048
Elad Moshe <eladmoshe@gmail.com>
2013-01-16 00:33:47 +0200

    basic impl

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
+if (process.argv.length < 2){
+	console.log("Usage: node server.js <dir name> <port> \rport and dir are optional");
+	return;
+}
 var dir = process.argv[2] || __dirname;
-
+var port = process.argv[3] || 1234
 var connect = require('connect');
 connect.createServer(
     connect.static(dir)
-).listen(1234);
+).listen(port);
+console.log("* Server is running at port ",port, "\r* serving directory ",dir);
